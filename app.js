c00672eedd7ab5bf6952bdab9476a7ae7773c952
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-01 12:18:54 +0500

    Добавить bodyParser и cors

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -7,4 +10,9 @@ const DATABASE_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
 });
+app.use(cors({
+  credentials: true,
+  origin: [/*'https://practical.mesto.students.nomoredomainsrocks.ru'*/'http://localhost:3001'],
+}));
+app.use(bodyParser.json());
 app.listen(PORT);
