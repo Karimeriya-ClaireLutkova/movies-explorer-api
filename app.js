1e3905f1809e649b98d250092addb8437be79c3b
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-02 00:06:06 +0500

    Добавить логирование

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const routerUsers = require('./routes/users');
 const routerMovies = require('./routes/movies');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 
@@ -16,11 +17,13 @@ const DATABASE_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 mongoose.connect(DATABASE_URL, {
   useNewUrlParser: true,
 });
+
 app.use(cors({
   credentials: true,
   origin: [/*'https://practical.mesto.students.nomoredomainsrocks.ru'*/'http://localhost:3001'],
 }));
 app.use(bodyParser.json());
+app.use(requestLogger);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -40,6 +43,7 @@ app.use(routerMovies);
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Ресурс не найден.'));
 });
+app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
