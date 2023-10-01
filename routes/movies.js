90bed76804216fbd4e9fc70fa56230acb9d1f8fd
Svetlana Lutkova <lilissiya@yandex.ru>
2023-10-02 02:35:19 +0500

    Исправить регулярное выражение

--- routes/movies.js
+++ routes/movies.js
@@ -15,8 +15,8 @@ router.post('/movies', celebrate({
     year: Joi.number().integer().required().min(1895)
       .max(2023),
     description: Joi.string().required(),
-    nameRU: Joi.string().required().regex(/[я-яё0-9.-]/gi),
-    nameEN: Joi.string().required().regex(/[a-z0-9.-]/gi),
+    nameRU: Joi.string().required().regex(/[я-яё0-9.-]/i),
+    nameEN: Joi.string().required().regex(/[a-z0-9.-]/i),
     image: Joi.string().required().regex(/^https?:\/\/(www\.)?([0-9a-zA-Z.-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.?(?:jpe?g|gif|png|bmp|webp)?$/i),
     trailerLink: Joi.string().required().regex(/^(http[s]?:\/\/(www\.)?[-a-zA-Z0-9@:%_+.~#?&/=]+)+\.?(?: mp4|mov|ts|mkv|avi|wmv)?/i),
     thumbnail: Joi.string().required().regex(/^https?:\/\/(www\.)?([0-9a-zA-Z.-]+\.)+[a-z]{2,6}(?:\/[^/#?]+)+\.?(?:jpe?g|gif|png|bmp|webp)?$/i),
