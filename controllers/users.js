1e562e4339231ab54050cec0d1519fe4967835ba
Svetlana Lutkova <lilissiya@yandex.ru>
2023-11-12 07:30:55 +0500

    Исправить техошибку

--- controllers/users.js
+++ controllers/users.js
@@ -15,10 +15,9 @@ module.exports.createUser = (req, res, next) => {
   User.findOne(email)
     .then((user) => {
       if (user) {
-        throw new ConflictError('Пользователь с таким email уже существует.');
+        next(new ConflictError('Пользователь с таким email уже существует.'));
       }
-    })
-    .catch(next);
+    });
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name,
